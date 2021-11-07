8e131c2a671a40b8be184f10b12892069d50b367
techie-akki <chweet.cmpl.aki@gmail.com>
2021-11-07 16:17:11 +0530

    implementaion of global styles

---
+++ styles/global.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const globalStyles = StyleSheet.create({
+  container: {
+    flex:1,
+    padding:24,
+  },
+  titleText: {
+    fontFamily: 'nunito-bold',
+    fontSize:20,
+    color:'#333',
+  },
+  paragrapgh: {
+    marginVertical:8,
+    lineHeight:20,
+  }
+});
