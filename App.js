eddf5a50e6baba61b1ed4c385ad35c83529ec2de
techie-akki <chweet.cmpl.aki@gmail.com>
2021-11-12 14:52:43 +0530

    drawer navigation implemented

--- App.js
+++ App.js
@@ -1,19 +1,45 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
 import * as Font from 'expo-font';
 import Home from './screens/home';
 import Review from './screens/review';
+import About from './screens/about';
 import AppLoading from 'expo-app-loading' ;
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+const Stack = createNativeStackNavigator();
+function HomeStack() {
+  return (
+    <Stack.Navigator initialRouteName="GameZone"
+    screenOptions={{
+      headerStyle: {
+        backgroundColor: '#ccc',
+      },
+      headerTintColor: '#444',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    }}
+    >
+      <Stack.Screen name="GameZone" component={Home} />
+      <Stack.Screen name="Review" component={Review}  />
+    </Stack.Navigator>
+  );
+}
+const Drawer = createDrawerNavigator();
+
+
 
 const getFonts = () => Font.loadAsync({
   'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
   'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
 });
 
-const Stack = createNativeStackNavigator();
+
 
 export default function App() {
   const [fontsLoaded, setFontsLoaded] = useState(false);
@@ -22,7 +48,7 @@ export default function App() {
     return (
       // <Home />
       <NavigationContainer>
-        <Stack.Navigator initialRouteName='Home'
+        <Drawer.Navigator 
         screenOptions={{
           headerStyle: {
             backgroundColor: '#ccc',
@@ -32,9 +58,9 @@ export default function App() {
             fontWeight: 'bold',
           },
         }}>
-          <Stack.Screen name ='Home' component={Home} options={{ title: 'GameZone'}} />
-          <Stack.Screen name ='Review' component={Review} />
-        </Stack.Navigator>
+          <Drawer.Screen name ='Home' component={HomeStack} options={{headerShown:false}} />
+          <Drawer.Screen name ='About' component={About} />
+        </Drawer.Navigator>
       </NavigationContainer>
        
     );
