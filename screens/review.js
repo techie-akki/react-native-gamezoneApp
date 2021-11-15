a04b0c5c7fe93ebef8c2d4284d8162774a98e072
techie-akki <chweet.cmpl.aki@gmail.com>
2021-11-15 16:22:42 +0530

    custom card component added

--- screens/review.js
+++ screens/review.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Text,} from "react-native";
 import { globalStyles } from "../styles/global";
+import Card from "../shared/card";
 
 export default function Review({route}){
 
@@ -8,10 +9,11 @@ export default function Review({route}){
 
   return (
     <View style = {globalStyles.container}>
-      
-      <Text style={globalStyles.titleText}>{route.params.title}</Text>
-      <Text style={globalStyles.titleText}>{route.params.body}</Text>
-      <Text style={globalStyles.titleText}>{route.params.rating}</Text>
+      <Card>
+        <Text style={globalStyles.titleText}>{route.params.title}</Text>
+        <Text style={globalStyles.titleText}>{route.params.body}</Text>
+        <Text style={globalStyles.titleText}>{route.params.rating}</Text>
+      </Card>
     </View>
   )
 }
